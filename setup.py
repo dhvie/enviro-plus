87cf1e27f01486b6b88bb06fce50bdfd7d7ddba0
dhvie <dh_vie@yahoo.com>
2020-04-25 00:12:13 +0100

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='enviro_mqtt',
-    version='0.1.5',
+    version='0.1.6',
     packages=['enviro_mqtt'],
     package_data={
         "enviro_mqtt": ['fonts/**/*']
