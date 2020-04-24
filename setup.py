ca03571a8dcd0337008bfb1a03154482a87a586a
dhvie <dh_vie@yahoo.com>
2020-04-24 23:56:00 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='enviro_mqtt',
-    version='0.1.3',
+    version='0.1.4',
     packages=['enviro_mqtt'],
     package_data={
         "enviro_mqtt": ['fonts/**/*']
